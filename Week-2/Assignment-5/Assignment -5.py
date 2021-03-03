3356f5013909b1eb30f478ebd8c6f4d447123b99
Poyu781 <v3708599@gmail.com>
2021-03-03 16:23:36 +0800

    modify week2 assignment5

--- Week-2/Assignment-5/Assignment -5.py
+++ Week-2/Assignment-5/Assignment -5.py
@@ -1,3 +1,4 @@
+# 原始寫法
 def binary_search_position(numbers, target):
     first_index = 0
     last_index = len(numbers)-1  # 原本為 len(numbers) ，後修改 -1 ，確保 last_index 不會超出 numbers 範圍
@@ -12,10 +13,11 @@ def binary_search_position(numbers, target):
                     continue
                 else :
                     middle_index = middle_index + 1
-            return middle_index
+                    return middle_index
+        elif target > numbers[len(numbers)-1] :
+            return len(numbers)
         # 當找不到 target 的時候，會從左右兩邊來判斷，哪邊最接近取哪邊，若相等則取較小 index 的
         elif first_index == middle_index or last_index == middle_index :
-            # 如果 target 比最後一個 index 的值還大，則會讓 numbers[last_index] - target 為負值，進而回傳 last_index
             if target - numbers[first_index] > numbers[last_index] - target:
                 return last_index
             else:
@@ -26,4 +28,20 @@ def binary_search_position(numbers, target):
         elif numbers[middle_index] < target :
             first_index = middle_index
             middle_index = (first_index+last_index)//2
-binary_search_position([1, 2, 5, 6, 7,11,15], 16)
+
+# 參照 bisect_left 的方式去仿寫
+
+def binary_search_first(numbers, target, start_index=0, last_index=None):
+    if start_index < 0:
+        raise ValueError('start_index must be non-negative')
+    if last_index is None:
+        last_index = len(numbers)
+
+    while start_index < last_index:
+        mid = (start_index + last_index) // 2
+        if numbers[mid] < target:
+            start_index = mid + 1
+        else:
+            last_index = mid 
+
+    return start_index
