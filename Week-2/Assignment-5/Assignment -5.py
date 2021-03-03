b88b3191ef21c37c44f5845d59294cf93a45aaef
Poyu781 <66525909+Poyu781@users.noreply.github.com>
2021-03-03 14:21:55 +0800

    Update Binary Search Bug
    修改找不到 target 時的判斷式，確保會給出正確的值。
    若比 index 0 還小，則回傳 0
    若比 index max 還大，則回傳 index max

--- Week-2/Assignment-5/Assignment -5.py
+++ Week-2/Assignment-5/Assignment -5.py
@@ -1,9 +1,9 @@
 def binary_search_position(numbers, target):
     first_index = 0
-    last_index = len(numbers)
+    last_index = len(numbers)-1  # 原本為 len(numbers) ，後修改 -1 ，確保 last_index 不會超出 numbers 範圍
     middle_index = (first_index+last_index)//2
     while True:
-        # 找到符合 Target 的 index 後，再持續往前去看有沒有相等的數字，來找到符合資格的最小 idex
+        # 找到符合 Target 的 index 後，再持續往前去看有沒有相等的數字，來找到符合資格的最小 index
         if numbers[middle_index] == target :
             for i in range(middle_index): 
             #應該有更好的迴圈設法，但我現階段用 middle_index 的 Range ，是確保一定能更找到 index 0
@@ -15,7 +15,8 @@ def binary_search_position(numbers, target):
             return middle_index
         # 當找不到 target 的時候，會從左右兩邊來判斷，哪邊最接近取哪邊，若相等則取較小 index 的
         elif first_index == middle_index or last_index == middle_index :
-            if numbers[first_index]-target > target-numbers[last_index]:
+            # 如果 target 比最後一個 index 的值還大，則會讓 numbers[last_index] - target 為負值，進而回傳 last_index
+            if target - numbers[first_index] > numbers[last_index] - target:
                 return last_index
             else:
                 return first_index
@@ -25,3 +26,4 @@ def binary_search_position(numbers, target):
         elif numbers[middle_index] < target :
             first_index = middle_index
             middle_index = (first_index+last_index)//2
+binary_search_position([1, 2, 5, 6, 7,11,15], 16)
