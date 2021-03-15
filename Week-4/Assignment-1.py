5a4474ef3317845b7f48b7dba1919b59b85eca8b
Poyu781 <v3708599@gmail.com>
2021-03-15 17:34:27 +0800

    finish Week4 Assignment1

---
+++ Week-4/Assignment-1.py
@@ -0,0 +1,20 @@
+import threading
+from time import sleep,time
+
+def do_job(number):
+    sleep(1)
+    print(f"Job {number} finished")
+# rewrite everything inside this main function and keep others untouched
+def main():
+    start_time = time()
+    for i in range(5):
+        x = threading.Thread(target=do_job,args=(i,))
+        x.start()
+    x.join()
+    print("f")
+    # 等到全部子 Thread 執行完後，才去計算時間
+    print(time()-start_time)
+
+main()
+
+
