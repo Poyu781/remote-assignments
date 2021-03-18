0720fda27571783914ca08d6fdb603715f937e76
Poyu781 <v3708599@gmail.com>
2021-03-19 00:54:28 +0800

    Adjust Week4 As3 -Python: split the Post Url -Js: make function to access

--- Week-4/Assignmet-3/appForAssignment3.py
+++ Week-4/Assignmet-3/appForAssignment3.py
@@ -6,54 +6,59 @@ import pymysql.cursors
 app = Flask(__name__)
 
 # 建立連接資料庫資訊
-connection = pymysql.connect(host='localhost',
-                            user='root',
-                            password='root123',
-                            database='assignment',
-                            cursorclass=pymysql.cursors.DictCursor)
+connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='root123',
+    database='assignment',
+    cursorclass=pymysql.cursors.DictCursor
+)
 
 @app.route("/")
 def home():
     return "Hello"
 
-@app.route('/homePage',methods=["POST", "GET"])
+@app.route('/homePage')
 def sign_page(): 
     return render_template("Assignment3.html")
 
 # 前端回傳資料與資料庫互動
-@app.route('/checkSignSituation', methods = ["POST","GET"])
-def checkSignSituation():
+@app.route('/signUp', methods = ["POST"])
+def signUp():
     # 從 Fetch 得到相對應的 data
     data = request.get_json()
-    print(data)
-    # 連接資料庫
     connection.connect()
-
     with connection:
         with connection.cursor() as cursor:
-            # 判斷是登入還是註冊
-            if data['status'] == 'signUp':
-                sql_search =  "SELECT id, email  FROM user WHERE email = %s"
-                cursor.execute(sql_search,(data['email'],))
-                if cursor.fetchone() :
-                    text = {"message" : "Email already Exist"}
-                    return json.dumps(text)
-                else:
-                    sql_input = "INSERT INTO user (email, password) VALUES (%s, %s)"
-                    cursor.execute(sql_input, (data['email'], data['password']))
-                    connection.commit()
-                    text = {"message" : "Access Success"}
-                    return json.dumps(text)
+            sql_search =  "SELECT id, email  FROM user WHERE email = %s"
+            cursor.execute(sql_search,(data['email'],))
+            if cursor.fetchone() :
+                text = {"message" : "Email already Exist"}
+                return json.dumps(text)
+            else:
+                sql_input = "INSERT INTO user (email, password) VALUES (%s, %s)"
+                cursor.execute(sql_input, (data['email'], data['password']))
+                connection.commit()
+                text = {"message" : "Access Success"}
+                return json.dumps(text)
 
-            elif data['status'] == 'signIn': 
-                sql_search =  "SELECT id, email  FROM user WHERE email = %s and password = %s"
-                cursor.execute(sql_search,(data['email'],data['password']))
-                if cursor.fetchone() :
-                    text = {"message" : "Access Success"}
-                    return json.dumps(text)
-                else:
-                    text = {"message" :"Email or Password is wrong ! "}
-                    return json.dumps(text)                    
+
+@app.route('/signIn', methods = ["POST"])
+def signIn():
+    data = request.get_json()
+    connection.connect()
+    with connection:
+        with connection.cursor() as cursor:
+            sql_search =  "SELECT id, email  FROM user WHERE email = %s and password = %s"
+            cursor.execute(sql_search,(data['email'],data['password']))
+            if cursor.fetchone() :
+                text = {"message" : "Access Success"}
+                return json.dumps(text)
+            else:
+                text = {"message" :"Email or Password is wrong ! "}
+                return json.dumps(text)  
+
+
 
 # 登入後的歡迎畫面
 @app.route('/welcomePage', methods = ["POST","GET"])
--- Week-4/Assignmet-3/static/js/signData.js
+++ Week-4/Assignmet-3/static/js/signData.js
@@ -1,6 +1,3 @@
-const form = document.querySelectorAll("form");
-const email = document.querySelectorAll('input[name="email"]');
-const password = document.querySelectorAll('input[name="password"]');
 const errorMessage = document.querySelector('.errorReturnMessage');
 
 function setAction(message, email) {
@@ -19,15 +16,15 @@ function setAction(message, email) {
     }
 }
 
-function fetchUser(email, password, status) {
+function fetchUser(url, email, password) {
     // 將資料送傳到後端，來做後續資料庫的比對與建檔
-    fetch('/checkSignSituation', {
+    fetch(url, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({email, password, status })
+        body: JSON.stringify({email, password })
     })
     // 後端會根據資料，給予相對應的訊息回前端
     .then(response => {
@@ -38,28 +35,17 @@ function fetchUser(email, password, status) {
     })
 }
 
-form.forEach( form => {
-    form.addEventListener('submit',(e) =>{
-        // 將 form 預設的 submit 效果移除
-        e.preventDefault();       
-        // 找出哪一個按鈕被點擊
-        let status = e.target.id
-        let emailValue ;
-        let passwordValue ;
-        // 根據點擊的按鈕，找出相對應的 Email and Password （這邊應該可以用更好的寫法）
-        if (status =="signUp"){
-            emailValue = email[0].value
-            passwordValue = password[0].value
-            console.log(emailValue)
-        }
-        else if(status =="signIn"){
-            emailValue = email[1].value
-            passwordValue = password[1].value
-            console.log(emailValue)            
-        }
-        fetchUser(emailValue, passwordValue, status)
-    })
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const form = e.target
+    console.log('form_name', form.id)
+    const email = form.querySelector('input[name="email"]').value
+    const password = form.querySelector('input[name="password"]').value
+    console.log({email, password})
+    fetchUser(`/${form.id}`, email, password)
 })
+
+
 
 
 
