d578efac0375b20957969610f448d26beb082dec
Poyu781 <v3708599@gmail.com>
2021-03-18 11:57:07 +0800

    Week4 As3 SignUp Finish

--- Week-4/Assignmet-3/appForAssignment3.py
+++ Week-4/Assignmet-3/appForAssignment3.py
@@ -1,69 +1,60 @@
-from flask import Flask, render_template, flash
-from flask import request,url_for,redirect,make_response
+from flask import (Flask, render_template, flash,
+request,url_for,redirect,make_response)
 import json
-app = Flask(__name__)
-
 import pymysql.cursors
+
+app = Flask(__name__)
 connection = pymysql.connect(host='localhost',
                             user='root',
                             password='root123',
-                            database='user',
+                            database='assignment',
                             cursorclass=pymysql.cursors.DictCursor)
-# Connect to the database
-def interactive(data):
-    with connection:
-        with connection.cursor() as cursor:
-        # Create a new record
-            sql_search =  "SELECT id, email  FROM aa WHERE email = %s"
-            cursor.execute(sql_search,(data['email'],))
-            if cursor.fetchone() :
-                # 加一個訊息，說這個 email 已經用過了
-                return "already exist"
-            else:
-                sql_input = "INSERT INTO aa (email, password) VALUES (%s, %s)"
-                cursor.execute(sql_input, (data['email'], data['password']))
 
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
-        
-        connection.commit()
-    connection.connect()
-
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello, My Server"
+    return "Hello"
 
-
-@app.route('/Homepage',methods=["POST", "GET"])
+@app.route('/homePage',methods=["POST", "GET"])
 def sign_page(): 
     return render_template("Assignment3.html")
 
 @app.route('/checksignin', methods = ["POST","GET"])
-def check_signin():
-
+def check_sign_up():
+    
     data = request.get_json()
+    print("1")
     print(data["email"], data["password"])
+
+    connection.connect()
+
     with connection:
         with connection.cursor() as cursor:
         # Create a new record
-            sql_search =  "SELECT id, email  FROM aa WHERE email = %s"
+            sql_search =  "SELECT id, email  FROM test WHERE email = %s"
             cursor.execute(sql_search,(data['email'],))
             if cursor.fetchone() :
                 # 加一個訊息，說這個 email 已經用過了
-                return "already exist"
+                text = {"message" : "Email already Exist"}
+                print(text)
+                return json.dumps(text)
             else:
-                sql_input = "INSERT INTO aa (email, password) VALUES (%s, %s)"
+                sql_input = "INSERT INTO test (email, password) VALUES (%s, %s)"
                 cursor.execute(sql_input, (data['email'], data['password']))
-
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
+                connection.commit()
+                connection.close
+                text = {"message" : "Access Success"}
+                print(text)
+                return json.dumps(text)
+                
         
-        connection.commit()
-    connection.connect()
+        
     
-    return "2"
+    
 
-
+@app.route('/welcomePage', methods = ["POST","GET"])
+def welcomePage(email=None):
+    email = request.args.get('email', email)
+    return f"hello, {email} ! Nice to meet you !"
 
 
 if __name__ == "__main__":
--- Week-4/Assignmet-3/static/js/signData.js
+++ Week-4/Assignmet-3/static/js/signData.js
@@ -1,19 +1,47 @@
-const signInClick = document.querySelector('.sigIn input[type="submit"]')
+const form = document.querySelector('form')
+const signUpButton = document.querySelector('.signUp input[type="submit"]')
+const signUpErrorMessage = document.querySelector('.errorReturnMessage')
 const email = document.querySelector('input[name="email"]')
 const password = document.querySelector('input[name="password"]')
-signInClick.addEventListener("click",() =>{
+signUpButton.style.backgroundColor = 'red';
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+})
+
+signUpButton.addEventListener('click',() =>{
     emailValue = email.value
     passwordValue = password.value
-    fetch("/checksignin", {
+    fetch('/checksignin', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({"email": emailValue, "password": passwordValue})
+        body: JSON.stringify({'email': emailValue, 'password': passwordValue})
 }   )
+    .then(response => {
+        return response.json()
+    })
+    .then(json => {
+        console.log(json['message'])
+        if (json['message'] === "Access Success"){
+            window.location.href = '/welcomePage?email='+emailValue ;
+        }
+        else if (json['message'] === "Email already Exist"){
+            signUpErrorMessage.innerText = "Email already Exist"
+        }
+        // else if (json['message'] === "Email or Password is wrong ! "){
+        //     signInErrorMessage.innerText = "Email or Password is wrong !"
+        // }
+    })
+
 
 })
 
-console.log("d")
+
+
+
+
+
 
